2175b57a798bb11e77e7a7e2ab442e0a0bee5f3b
SimonTurner-DiMe <simon.turner@dimesociety.org>
2024-11-26 18:02:11 +0000

    Update Final Simulation for Manuscript.R
    Internal working notes removed

--- Final Simulation for Manuscript.R
+++ Final Simulation for Manuscript.R
@@ -9,14 +9,13 @@ library(rsimsum)
 #### ####
 
 #### FINAL VERSION USED FOR MANUSCRIPT. #### 
-#Attempt 5 - Including a new MLR(DHT_bar,wCRO + wPRO).
 Design_5 <- createDesign(N = c(35,100,200,1000), meas_error_mag = c(0.5,1,1.5,2),
                          n_assess = c(1,3,5,7),missing_method = c('None', 'MNAR','MCAR'), missing_rate = c(0,0.1,0.25,0.4),
                          subset = !((missing_method == 'None' & missing_rate != 0) | (missing_method != 'None' & missing_rate == 0)))
 
 Generate_with_latent_vals <- function(condition,fixed_objects=NULL) {
   
-  #Use Attach if it's quicker
+
   Attach(condition)
   #### DHT_Data ####
   
@@ -27,26 +26,19 @@ Generate_with_latent_vals <- function(condition,fixed_objects=NULL) {
   latent_effect = 1250
   
   thetas<- Generate_Daily_Thetas(N,fluct_sd)
-  #with(condition,Generate_Daily_Thetas(N,fluct_sd))
-  
+   
   DHT_raw <- Generate_DHT_Data_Full_with_filtered_thetas(thetas,N,fluct_sd,meth_filt_sd,base_rate,latent_effect,meas_error_mag)
-  #with(condition,Generate_DHT_Data_Full(thetas,N,fluct_sd,meth_filt_sd,base_rate,latent_effect,meas_error_mag))
   
   #apply missingness condition
   if (missing_method == "MCAR") {
-    #(condition$missing_method == "MCAR") {
-    
+       
     DHT_data <- Apply_MCAR_Missing_Data_with_latents(DHT_raw,missing_rate)
-    #DHT_data<- sapply(DHT_raw[8:15],add_missing,rate=missing_rate)
-    #sapply(DHT_raw,add_missing,rate=condition$missing_rate)
-    
+     
   } else if (missing_method == "MNAR") {
-    #(condition$missing_method == "MNAR") {
-    
+        
     DHT_data <- data.frame(DHT_raw[1:7],
                            delete_MNAR_1_to_x(DHT_raw[8:14],
                                               missing_rate,
-                                              #condition$missing_rate,
                                               c("DHT_day_1", "DHT_day_2", "DHT_day_3",
                                                 "DHT_day_4", "DHT_day_5", "DHT_day_6", "DHT_day_7"),
                                               x=4)
@@ -61,8 +53,7 @@ Generate_with_latent_vals <- function(condition,fixed_objects=NULL) {
   
   #Vector of days to include in assessment (DHT-side only; COAs always use 7 days)
   days_to_include <- c((8-n_assess):7)
-  #with(condition,c((8-n_assess):7))
-  
+    
   # Calc the DHT_mean data based on which assessment days are being included in the data collection ####
   if (n_assess != 1) {
     
@@ -85,15 +76,14 @@ Generate_with_latent_vals <- function(condition,fixed_objects=NULL) {
   
   #Generate the mean latent trait over the assessment period for the Weekly PROS.
   #We're using 7 previous days in every case, no matter the number of DHT assessment days included,
-  #as this is the typical period for COA's.
+  #as this is the typical period for COAs.
   days_to_include <- c(1:7)
   thetas_bar <- Generate_avg_thetas(thetas,days_to_include)
-  #hist(thetas_bar)
-  
+   
   #### Main PRO Data ####
   
   #Set perception filter
-  per_filt_sd = 1       #0.75 #0.5       #Varying this affects the strength of the day-to-day correlations
+  per_filt_sd = 1      #Varying this affects the strength of the day-to-day correlations
   # THE PERCEPTION FILTER IS NOT ASSUMED TO RANDOMLY CHANGE FROM DAY TO DAY.
   # THIS IS A 'FIXED' FILTER.
   
@@ -109,61 +99,50 @@ Generate_with_latent_vals <- function(condition,fixed_objects=NULL) {
   #### Weekly ClinRO Data ####
   
   per_Filt_sd = 1 #0.5#0.25
-  #b0 = c(-1.0, -0.6, -0.2, 0, 0.2, 0.6, 1.0)
+  
   b0 = c(-0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75) #Brought the tails in
-  # and made the transitions uniform to give n=35 a shot of converging with CFA
-  reliability = 0.7     #Need a more nuanced way of using this to determine the alpha parameters
-  #Currently coming out at 0.9 - fixed
+  # and made the transitions uniform 
+  reliability = 0.7    
+ 
   
   ClinRO_Data <- Simulate_5_7_ClinRo_IRT_Data_return_latents(thetas_bar,per_Filt_sd,N,b0,reliability)
-  #ClinRO_Data <- Simulate_5_7_ClinRo_IRT_Data_Full(thetas_bar,per_Filt_sd,N,b0,reliability)
-  #with(condition, Simulate_5_7_ClinRo_IRT_Data_Full(thetas_bar,per_Filt_sd,N,b0,reliability))
-  
+    
   ####               ####
   
-  #### Daily Single-item PRO (reliability = 0.6??? No, alpha makes no sense for single item)####
-  per_filt_sd = 0.5 #0.75
+  #### Daily Single-item PRO ####
+  per_filt_sd = 0.5 
   
   # Create iMIC distribution and other thresholds
-  imic <- rnorm(N, 0.5, 0.075) #with(condition,rnorm(N, 0.5, 0.075))
-  thrd1 <- rnorm(N, -1.5, 0.075)#with(condition,rnorm(N, -1.5, 0.075))
-  thrd2 <- rnorm(N, -0.5, 0.075)#with(condition,rnorm(N, -0.5, 0.075))
+  imic <- rnorm(N, 0.5, 0.075) 
+  thrd1 <- rnorm(N, -1.5, 0.075)
+  thrd2 <- rnorm(N, -0.5, 0.075)
   thrd3 <- imic
-  thrd4 <- rnorm(N, 1.5, 0.075)#with(condition,rnorm(N, 1.5, 0.075))
+  thrd4 <- rnorm(N, 1.5, 0.075)
   
   thrds<- data.frame(thrd1,thrd2,thrd3,thrd4)
-  #as.data.frame(cbind(thrd1,thrd2,thrd3,thrd4))
-  
+   
   #Sim the data
   Daily_PRO <- Sim_Daily_Single_Item_return_latents(thetas,N,thrds,per_filt_sd)
-  #Sim_Daily_Single_Item(thetas,N,thrds,per_filt_sd)
-  #with(condition,Sim_Daily_Single_Item(thetas,N,thrds,per_filt_sd))
+
   
   
-  ##########################
-  #REVISIT THIS TOMORROW - vary both of included days in mean and CFA model, or neither
-  ##########################
+
   #Vector of days to include in assessment (DHT-side only; COAs always use 7 days)
   days_to_include <- c(1:7)
-  #c((8-n_assess):7)
-  #with(condition,c((8-n_assess):7))
-  
+   
   
   #Calc the Daily PRO mean data based on which assessment days are being included in the data collection
   if (n_assess != 1) {
-    #(condition$n_assess != 1) {
     Daily_PRO_means <- rowMeans(Daily_PRO[,7+days_to_include])
   } else {
     Daily_PRO_means <- Daily_PRO[,14]
   }
-  #For comparison
-  #Full_Week_dPRO_means<- rowMeans(Daily_PRO[,c(1:7)])
+
   ####              ####
   
   # Calc the perception-filtered latent traits means based on which assessment days
   #are being included in the data collection
   if (n_assess != 1) {
-    #(condition$n_assess != 1) {
     filtered_dPRO_latent_Means <- rowMeans(Daily_PRO[,days_to_include],na.rm = TRUE)
   } else {
     filtered_dPRO_latent_Means <- Daily_PRO[,7]
@@ -174,8 +153,7 @@ Generate_with_latent_vals <- function(condition,fixed_objects=NULL) {
   dat<-data.frame(thetas, DHT_data,filtered_DHT_latent_Means,DHT_Means,
                   thetas_bar,Weekly_PRO,ClinRO_Data,Daily_PRO,filtered_dPRO_latent_Means,
                   Daily_PRO_means,subject=c(1:N))
-  # dat<-data.frame(thetas, DHT_data,DHT_Means,
-  #                 Weekly_PRO,ClinRO_Data,Daily_PRO,Daily_PRO_means)
+
   
   names(dat)[1:7]<-c("theta_day_1","theta_day_2","theta_day_3","theta_day_4",
                      "theta_day_5","theta_day_6","theta_day_7")
@@ -199,8 +177,7 @@ Analyse_with_latents_additional_MLR <- function(condition, dat, fixed_objects = 
   #DHT average data against the weekly_PRO#
   wPRO_regr <- lm(DHT_Means~Weekly_PRO,data=dat)
   wPRO_regr_R2 <- summary(wPRO_regr)$r.squared
-  #as.numeric(summary(PRO_regr)$r.squared)
-  
+   
   wPRO_latent_regr<- lm(filtered_DHT_latent_Means~filtered_latents,data=dat)
   wPRO_latent_regr_R2<- summary(wPRO_latent_regr)$r.squared
   
@@ -214,7 +191,6 @@ Analyse_with_latents_additional_MLR <- function(condition, dat, fixed_objects = 
                                   +filtered_dPRO_latent_Means,data=dat)
   
   #Use each day of daily PRO as a separate variable
-  #Revisit tomorrow - Should all 7 dPRO days be used when n_assess <7?
   regr_all_daybyday <- lm(DHT_Means~Weekly_PRO+Weekly_ClinRO
                           +PRO_day_1+PRO_day_2+PRO_day_3+PRO_day_4+PRO_day_5+PRO_day_6+PRO_day_7,data=dat)
   
@@ -263,31 +239,25 @@ Analyse_with_latents_additional_MLR <- function(condition, dat, fixed_objects = 
   #### Model definition ####
   
   if (condition$n_assess == 7) {
-    ####Aim 1####
-    
+       
     sim.model <- '
                DM   =~ DHT_day_1 + DHT_day_2 + DHT_day_3 + DHT_day_4 + DHT_day_5 + DHT_day_6 + DHT_day_7
                wPRO =~ Item_1 + Item_2 + Item_3 + Item_4 + Item_5 + Item_6 + Item_7 + Item_8 + Item_9 + Item_10 + Item_11 + Item_12'
     
   } else if (condition$n_assess == 5) {
-    
-    ####Aim 1####
-    
+   
     sim.model <- '
                DM   =~ DHT_day_3 + DHT_day_4 + DHT_day_5 + DHT_day_6 + DHT_day_7
                wPRO =~ Item_1 + Item_2 + Item_3 + Item_4 + Item_5 + Item_6 + Item_7 + Item_8 + Item_9 + Item_10 + Item_11 + Item_12'
     
   } else if (condition$n_assess == 3) {
-    
-    ####Aim 1####
-    
+     
     sim.model <- '
                DM   =~ DHT_day_5 + DHT_day_6 + DHT_day_7
                wPRO =~ Item_1 + Item_2 + Item_3 + Item_4 + Item_5 + Item_6 + Item_7 + Item_8 + Item_9 + Item_10 + Item_11 + Item_12'
     
   } else if (condition$n_assess == 1) {
     
-    ####Aim 1####
     
     sim.model <- '
                DM   =~ DHT_day_7
@@ -298,7 +268,6 @@ Analyse_with_latents_additional_MLR <- function(condition, dat, fixed_objects = 
   #### ####
   
   #### Fit the models ####
-  # Aim 1 #
   
   try(
     {
@@ -378,8 +347,7 @@ Summarise_with_latents_ignore_large_CFA_vals <- function(condition, results, fix
       
       
       "cfa1_cor_Mean" = mean(results$Aim_1_cor,na.rm=TRUE),
-      "cfa1_cor_SE" = sqrt(var(results$Aim_1_cor[abs(results$Aim_1_cor)<=2],na.rm=TRUE)),#This should be <=1 !!!
-      #"cfa1_cor_SE" = sqrt(var(results$Aim_1_cor,na.rm=TRUE)),
+      "cfa1_cor_SE" = sqrt(var(results$Aim_1_cor[abs(results$Aim_1_cor)<=2],na.rm=TRUE)),
       "cfa1_Mean_Emp_bias" = mean(results$Aim_1_cor - results$Avg_filtered_latents,na.rm=TRUE),
       
       "cfa1_cfi_Mean" = mean(results$Aim_1_cfi,na.rm=TRUE),

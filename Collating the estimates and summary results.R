cbcc003bc178fcba3e23d0ab37f4f6b980c51ed4
SimonTurner-DiMe <simon.turner@dimesociety.org>
2024-11-30 16:54:00 +0000

    Update Collating the estimates and summary results.R
    Tidying

--- Collating the estimates and summary results.R
+++ Collating the estimates and summary results.R
@@ -1,19 +1,16 @@
 library(ggplot2)
 library(tidySEM)
 
-#Choose data set
+####Collate the estimates from each simmulation into one grand data frame, 
+####and write the estimates data frame and summary results to CSV.
+
 data_set <- Final_Results
-  #Absolutely_Final_no_CFA_2
-  #Absolutely_Final_wCRO_alpha_07
 
-# dir_name <- file.choose()
-# dir_name<-dirname(dir_name)
-
-#Import estimates from first condition
+#Import estimates from first condition. 
+#Estimates from each condition are saved locally based on your workspace settings.
 final_sim_res_1<- SimResults(data_set,1)
-  #readRDS(paste(dir_name,"/results-row-",1,".rds",sep=""))                
 
-
+#Append simulation condition parameters to estimates
 estimates_data_1<- with(final_sim_res_1,
                        data.frame(N=rep(condition$N,nrow(results)),
                                   meas_error_mag = rep(condition$meas_error_mag,nrow(results)),
@@ -23,15 +20,15 @@ estimates_data_1<- with(final_sim_res_1,
                        
 )
 
-#grand estimate data.frame
+#Add estimates and condition info from condition 1 to the grand data.frame of estimates
 collated_estimates <- estimates_data_1
 
-#loop through remaining conditions, adding estimates to the grand estimate data.frame                          
+#loop through remaining simulation design conditions, adding estimates 
+# and condition information to the grand data.frame of estimates.                          
 for (i in 2:nrow(data_set)) {
   
   nam <- paste("final_sim_res_",i,sep="")
   
-  #assign(nam,readRDS(paste(dir_name,"/results-row-",i,".rds",sep="")))
   assign(nam,SimResults(data_set,i))
   
   nam <- paste("estimates_data_",i,sep="")
@@ -46,15 +43,18 @@ for (i in 2:nrow(data_set)) {
   
   collated_estimates <- rbind(collated_estimates, eval(parse(text=nam)))
   
-  #Keep the workspace tidy
+  #Keep the workspace tidy while looping by removing unneeded variables
   rm(list = ls(pattern = paste("final_sim_res_",i,sep="")))  
   rm(list = ls(pattern = nam))
   
 }
 
+#User is prompted to choose a directory to save the outputs to.
 dir_name <- file.choose()
 dir_name<-dirname(dir_name)
 
+#Write the grand data frame of estimates to CSV
 write.csv(collated_estimates, paste(dir_name,"/Collated Estimates.csv",sep=""))
 
+#Write the simulation performance measures summary to CSV.
 write.csv(data_set,paste(dir_name,"/Summarised results.csv",sep=""))
